0b9bd4908ac52ee10669bde58508de25c91dee73
Kelsie Harris <kharris835@gmail.com>
2023-02-08 21:28:13 -0700

    Bug: logo error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="text-center">
+        Dictionary
       </header>
     </div>
   );
