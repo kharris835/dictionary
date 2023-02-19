755dfc5fe3f8ed212108bd404a7ce43f5c1573fd
Kelsie Harris <kharris835@gmail.com>
2023-02-18 17:34:38 -0700

    feat: add axios and make call to api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
-  function search(event) {
-    event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
-  }
-
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    // alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
