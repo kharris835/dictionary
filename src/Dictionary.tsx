5d7238de75db13070368ad3e353e54b6dff60b49
Kelsie Harris <kharris835@gmail.com>
2023-02-21 17:52:22 -0700

    feat: convert to TSX

--- src/Dictionary.tsx
+++ src/Dictionary.tsx
@@ -21,7 +21,6 @@ export default function Dictionary() {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
