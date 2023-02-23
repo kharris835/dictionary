11aef00fcd92eddfead105a8f4afdec67e6db847
Kelsie Harris <kharris835@gmail.com>
2023-02-23 14:25:52 -0700

    feat(Photos): flex images

--- src/Photos.tsx
+++ src/Photos.tsx
@@ -6,15 +6,15 @@ export default function Photos(props) {
   if (props.photos) {
     return (
       <div className="">
-        <div className="row mt-2 mb-2">
+        <div className="row mt-2 mb-2 g-3">
           {props.photos.map(function (photo, keyword) {
             return (
-              <div className="col-4" key={photo.id}>
+              <div className="col-md-4" key={photo.id}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
                     alt={keyword}
-                    className="img-fluid mt-2 mb-2 photo"
+                    className="img-fluid photo"
                   />
                 </a>
               </div>
