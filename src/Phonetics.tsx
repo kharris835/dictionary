168749aff794bd5d62a71f7802bb79ff9f8359ec
Kelsie Harris <kharris835@gmail.com>
2023-02-23 09:50:15 -0700

    style: update colors and spacing

--- src/Phonetics.tsx
+++ src/Phonetics.tsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function Phonetics(props) {
   return (
     <span className="me-1">
-      {props.phonetic.text}
       <audio
-        className="ms-5"
+        className=""
         controls
         controlsList="nodownload noplaybackrate"
         src={props.phonetic.audio}
       ></audio>
+      {props.phonetic.text}
     </span>
   );
 }
