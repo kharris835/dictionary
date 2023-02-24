332b6ea29dcb973cd06181ed6383d0d8213bf7de
Kelsie Harris <kharris835@gmail.com>
2023-02-24 08:34:19 -0700

    fix: phonetics text wrapping bug

--- src/Phonetics.tsx
+++ src/Phonetics.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetics(props) {
   return (
-    <span className="me-1">
+    <span className="me-1 ms-1">
       <audio
         className=""
         controls
         controlsList="nodownload noplaybackrate"
         src={props.phonetic.audio}
       ></audio>
-      {props.phonetic.text}
+      <span className="phoneticText">{props.phonetic.text}</span>
     </span>
   );
 }
